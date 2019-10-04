a047b197bc7dca221baa75b8151a114bfc51dee8
Nova <novahasley@gmail.com>
2019-10-04 16:37:17 -0500

    Add title

--- src/components/create-user.js
+++ src/components/create-user.js
@@ -2,61 +2,62 @@ import React, {Component} from 'react'
 import axios from 'axios'
 
 export default class CreateUser extends Component {
-    constructor(props) {
-        super(props) 
+  constructor(props) {
+    super(props)
 
-        this.onChangeUsername = this.onChangeUsername.bind(this)
-    
-        this.onSubmit = this.onSubmit.bind(this)
+    this.onChangeUsername = this.onChangeUsername.bind(this)
 
-        this.state = {
-          username: ""
-        }
+    this.onSubmit = this.onSubmit.bind(this)
+
+    this.state = {
+      username: ""
     }
+  }
 
-    onChangeUsername(e) {
-        this.setState({
-            username: e.target.value
-        })
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const user = {
+      username: this.state.username
     }
+    console.log(user)
 
-    onSubmit(e) {
-        e.preventDefault()
+    axios.post('http://localhost:5000/users/add', user).then(res => console.log(res.data))
 
-        const user = {
-            username: this.state.username
-        }
-        console.log(user)
-
-        axios.post('http://localhost:5000/users/add', user).then(res => console.log(res.data))
-
-        this.setState({
-            username: ''
-        })
-    }
-    render() {
-        return (
-          <div>
-            <form onSubmit={this.onSubmit}>
-              <div className="form-group">
-                <label>Username: </label>
-                <input
-                  type="text"
-                  required
-                  className="form-control"
-                  value={this.state.username}
-                  onChange={this.onChangeUsername}
+    this.setState({
+      username: ''
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  
+                type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
                 />
-              </div>
-              <div className="form-group">
-                <input
-                  type="submit"
-                  value="Create User"
-                  className="btn"
-                />
-              </div>
-            </form>
           </div>
-        );
-    }
+          <div className="form-group">
+            <input
+                type="submit"
+                value="Create User"
+                className="btn" 
+                />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
