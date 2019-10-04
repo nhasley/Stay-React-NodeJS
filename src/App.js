0b4d2373663e7d15639b3ecd7a2192f8f30497f6
Nova <novahasley@gmail.com>
2019-10-04 10:47:47 -0500

    added nav and set up other components. rendered set-up for house list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Nav from './components/nav'
+import HouseList from './components/house-list'
+import EditHouse from './components/edit-house.js'
+import CreateHouse from './components/create-house.js'
+import CreateUser from './components/create-user.js'
+
 
 function App() {
   return (
     <Router>
-    
+    <Nav />
+    <br />
+    <Route path='/' exact component={HouseList}/>
+    <Route path='/edit/:id' exact component={EditHouse}/>
+    <Route path='/create' exact component={CreateHouse}/>
+    <Route path='/user' exact component={CreateUser}/>
     </Router>
   );
 }
